7f76e5dc27abd82ac5df7b3ade3d1b29e984327b
Clark Rinker <cgrinker@gmail.com>
2024-10-16 13:24:31 -0700

    Initial Commit

---
+++ Hear2There/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Hear2There
+//
+//  Created by clark rinker on 10/16/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
